251310f01f496f30e8718b5c8a9ceffc17dff82f
pavelYankov <prazor@abv.bg>
2017-07-26 14:06:07 +0300

    Add files via upload

---
+++ BattleGround/BattleUnits/UnitContracts/IBattleUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBattleGround
+{
+    public interface IBattleUnit: IHeal, IMove, ITakeDamage
+    {
+        int Health { get; }
+        int Damage { get; }
+        CurrentUnitPossition Possition { get;  }
+    }
+}
